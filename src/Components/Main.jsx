d3ffb6a1b01452ff71abf4360ea3b9678c0c7bed
kaldelyne <150160558+kaldelyne@users.noreply.github.com>
2024-04-05 16:02:04 -0400

    PTAC-AULA-05
    Salvando dados

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,18 +1,65 @@
 import { useState } from "react";
 
 export default function Main(){
-  
+    const [nome, setNome] = useState("");
+    const [telefone, setTelefone] = useState(0);
+    const [listaContatos, setListacontatos] = useState([]);
+    const [estado, setEstado] = useState("");
+
+    const registrar = (event) =>{
+      event.preventDefault();
+      alert("Deu certo");
+      setListacontatos([...listaContatos,
+      {
+        nomeSalvo: nome,
+        telefoneSalvo: telefone,
+        estadoSalvo: estado,
+      }
+    ]);
+    };
+
+    console.table(listaContatos);
+
     return(
       <main>
-        <form>
-            <input 
-            type="text" 
-             nome="" 
-             id="" 
-             onChange={(event)=> setNNome(event.target.value)}/>
+        <form action="" onSubmit={registrar}>
+            <label htmlFor="nome">Nome:</label>
+            <input
+            type="text"
+            nome="nome-contato"
+            id="nome"
+            onChange={
+            (event)=> setNome(event.target.value)}/>
+            
 
-             {nome}
+            <label htmlFor="telefone">Telefone:</label>
+            <input type="tel"
+            name="telefone-contato"
+            id="telefone"
+            value={telefone}
+            onChange={
+            (event)=> setTelefone(event.target.value)}/>
+           
+
+            <label htmlFor="estado">Estado:</label>
+            <input type="text"
+            nome="estado-contato"
+            id="estado"
+            value={estado}
+            onChange={
+            (event)=> setEstado(event.target.value)}/>
+            
+            <input type="reset" value="Apagar" />
+            <button> Salvar </button>
         </form>
+         
+        {
+      listaContatos.map((telefone, index )=>
+        <div key={index} >
+              <p>{telefone.nomeSalvo}</p>
+              <p>{telefone.telefoneSalvo}</p>
+              <p>{telefone.estadoSalvo}</p>
+        </div>)}
       </main>
     );
 }
